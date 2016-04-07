477abcb4e8da7d8cfcc688fca7cb851b37db9418
Laura Gordon <lgordon@hannaandersson.com>
2016-04-06 17:04:26 -0700

    Updated header styles and tritium

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -10,15 +10,38 @@
 
     move_here("//div[@id='Header']") {
       attribute("data-ur-set", "toggler")
-      add_class("mw-header")
-      
-      move_here("/div[@id='Logo']") {
-        # Move top menu into #Logo container
-         move_here("../../../div[@id='TopMenu']", "bottom") {
+       
+      move_here("//div[@id='Logo']") {
+        attribute("style", "overflow:hidden")
+        $("//div[@id='LogoContainer']"){
+          attribute("style", "float:left;")
+        }
+        
+        # Move top menu into #Logo container        
+         move_here("//div[@id='TopMenu']", "bottom") {
+            attribute("style", "float:right;")
             # Remove unwanted top menu items from upper menu
             remove(".//li[not(contains(@class, 'CartLink') or contains(@class, 'First'))]")
+            
+            #Change Account link image            
+            $(".//li[@class='First']"){
+              attribute("style", "list-style-type:none; display:inline-block;")
+              $(".//a"){
+                text("")
+                add_class("sprites-user mw_logo_btn")
+              }
+            }
+            # Change cart button background image
+            $(".//li[contains(@class, 'CartLink')]"){
+              attribute("style", "list-style-type:none; display:inline-block;")
+              $(".//a"){
+                text("")
+                add_class("sprites-cart mw_logo_btn")
+              }
+            }
          }
       }
+
       
       # Insert HTML for bottom of header
       insert("div", class: "mw_header_bottom") {
@@ -43,24 +66,26 @@
               attribute("placeholder", "Search...")
             }
           }
-        }
-      }
-      
-      # Move menu items into header
-      move_here("../div[@id='Menu']", "bottom") {
-        attribute("data-ur-toggler-component", "content")
-        # Move "Category List" element on bottom of #Menu element
-        move_here("../../div[@id='Wrapper']/div[@class='Left']/div[@id='SideCategoryList']", "bottom")
-        $$("li") {
-          add_class("mw_bar2")
-          $$("a"){
-            insert("div", class: "sprites-menu_go")
+
+          # Move menu items into header
+          move_here("//div[@id='Menu']", "bottom") {
+            attribute("data-ur-toggler-component", "content")
+            
+            # Move "Category List" element on bottom of #Menu element
+            move_here("//div[@id='Wrapper']/div[@class='Left']/div[@id='SideCategoryList']", "bottom")
+            move_here("li") {
+              add_class("mw_bar2")
+              $$("a"){
+                insert("div", class: "sprites-menu_go")
+              }
+            }
+            $$("ul") {
+              #add_class("sprites-menu_go")
+            }
           }
-        }
-        $$("ul") {
-          #add_class("sprites-menu_go")
-        }
-      }
-    }
-   }
+        }        
+      }      
+    }   
+  }
  }
+ 
